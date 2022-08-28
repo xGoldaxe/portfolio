57857dc031a2154fe759554bd96ac92ebceecb45
Pierre LEVEQUE <60315913+xGoldaxe@users.noreply.github.com>
2022-08-28 11:17:20 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -20,16 +20,15 @@ export default function About({handleTransition, transitionOver}) {
 				<span className={'about__text__almostTitle'}>Looking for a developper? </span>
 				Im freelance and I can help you to build your dream
 				interface, or connect you to an API or wathever you need! Student from 42 Paris,
-				I've build a strong background in computer science and low level developpment (
-				c and c++ ), and got specialized in Front-end developement with the newer
+				I acquired a strong background in computer science and low-level development (
+				C and C++ ), and specialized in Front-end developement with the lattest
 				technologies like react and nextjs.
 				</p>
 				<p>
-				You can take see my differents projects here, or take a look at my github.
+				You can see my different projects here, or take a look at my github
 				</p>
-				<p>
-				For more informations about my technical stack you can contact me on
-				linkedin, discord or mail.
+				For more information about my technical stack you can contact me on
+				linkedin, discord or email.
 				</p>
 				<div className='about__line about__text__line'></div>
 				<img src='/image/textDecoration.svg' alt='' className='about__textDecoration'/>
